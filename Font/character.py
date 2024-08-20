51ee9fde412a5eba5f4103dc83669ec70b4cb835
ivanrj7j <ivanrj7j@gmail.com>
2024-08-20 21:40:52 +0530

    character.py: bug fixes and new calculateDimension method

--- Font/character.py
+++ Font/character.py
@@ -2,8 +2,9 @@ from freetype import Glyph
 import numpy as np
 
 class Character:
-    def __init__(self, glyph:Glyph) -> None:
+    def __init__(self, glyph:Glyph, space:int) -> None:
         self.glyph = glyph
+        self.space = space
 
 
     def getBitmap(self):
@@ -16,15 +17,32 @@ class Character:
 
         diff = height - top
 
-        descent = max(0, diff)
-        ascent = min(0, diff)
+        bottomShift = max(0, diff)
+        topShift = min(0, diff)
 
-        return bitmap.buffer, (width, height), (ascent, descent)
+        return bitmap.buffer, (width, height), (topShift, bottomShift)
     
     def getCharacter(self):
-        buffer, (width, height), (ascent, descent) = self.getBitmap()
+        buffer, (width, height), (topShift, descent) = self.getBitmap()
         char = np.array(buffer).reshape((height, width))/255
 
-        baseLine = height + ascent
+        if len(buffer) == 0:
+            char = np.zeros((1, self.space))
+            return char, (self.space, 1), (topShift, descent)
 
-        return char, (width, height), (ascent, descent), baseLine
+        charSpace = np.zeros((height, self.space))
+        char = np.hstack((char, charSpace))
+
+        height, width = char.shape
+
+        return char, (width, height), (topShift, descent)
+    
+    def calculateDimension(self):
+        bitmap = self.glyph.bitmap
+
+        height = bitmap.rows
+        width = bitmap.width + self.space
+        diff = height - self.glyph.bitmap_top
+
+
+        return (width, height), (max(0, diff), min(0, diff))
